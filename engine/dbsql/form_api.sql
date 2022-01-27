31e84d97c46d536cb1f41f4e216a4b5687d7fd53
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-01-28 00:21:06 +0800

    Update form_api.sql
    Fixed the create db bug

--- engine/dbsql/form_api.sql
+++ engine/dbsql/form_api.sql
@@ -16,7 +16,7 @@
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-create database torro_api
+-- create database torro_api
 -- ========================================================
 -- design structure
 -- ----------------------------
