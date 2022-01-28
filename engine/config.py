81e9c21703e043978e05a189241e373992ce271f
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-01-28 16:46:28 +0800

    Update config.py
    clean up

--- engine/config.py
+++ engine/config.py
@@ -6,12 +6,6 @@ import configparser
 
 
 class Configuration():
-    """
-    读取数据库配置文件中的的用户信息,密码信息，主机信息，端口信息,数据库名称信息
-    config_path: 是配置文件的路径
-    """
-    COPYRIGHT = '版权所有 ©2018-2019 成都原子数据科技有限公司.'
-    EMAIL = 'surport@atomdatatech.com'
 
     def __init__(self, config_file_path='./config.ini'):
         try:
@@ -24,80 +18,33 @@ class Configuration():
             print(str(e))
 
     def write_file(self):
-        """
-        保存对配置文件的修改
-        :return:
-        """
         self.conf.write(open(self.config_file_path, 'r+'))
 
     def get_version(self):
-        """
-        获取版本信息
-        :return:
-        """
         return self.conf.get('Version', 'version')
 
     def get_build(self):
-        """
-        获取build信息
-        :return:
-        """
         return self.conf.get('Version', 'build')
 
-    @staticmethod
-    def get_company_email(self):
-        """
-        获取公司邮箱
-        :return:
-        """
-        return Configuration.EMAIL
-
-    @staticmethod
-    def get_copyright(self):
-        """
-        获取版权信息
-        :return:
-        """
-        return Configuration.COPYRIGHT
-
     def get_database_name(self, database_section='DB'):
-        """
-        获取数据库的名称
-        :param database_section:配置文档区域名称 如IPCONFIG、DB1
-        :return:
-        """
+
         if self.conf.has_section(database_section):
             return self.conf.get(database_section, 'name')
         return None
 
     def get_option(self, section, option):
-        """
-        获取配置文件选项
-        :param section:
-        :param option:
-        :return:
-        """
+
         return self.conf.get(section, option)
 
     def set_option(self, section, option, value):
-        """
-        设置配置文件选项
-        :param section:配置文档区域名称 如IPCONFIG、DB1
-        :param option:配置文档区域中选项名称，如name、host
-        :param value: 选项的值
-        :return:
-        """
+
         # 修改选项
         self.conf.set(section, option, value)
         # 保存修改
         self.write_file()
 
     def get_database_configuration(self, database_section):
-        """
-        获取数据库连接详细信息
-        :param database_section:配置文档区域名称 如IPCONFIG、DB1
-        :return:
-        """
+
         return {
             'name': self.conf.get(database_section, 'name'),
             'type':self.conf.get(database_section, 'type'),
@@ -108,11 +55,6 @@ class Configuration():
         }
 
     def get_start_config(self, section='IPCONFIG'):
-        """
-        获取启动配置
-        :param role_section:
-        :return:
-        """
 
         host = self.conf.get(section, 'host')
         port = self.conf.get(section, 'port')
