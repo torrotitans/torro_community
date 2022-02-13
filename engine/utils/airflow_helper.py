6439ee9a24fdc930c907f5c082c88a631a907cbe
torrotitans <torrotitans>
2022-02-13 11:20:07 +0800

    debug airflow trigger

--- engine/utils/airflow_helper.py
+++ engine/utils/airflow_helper.py
@@ -9,8 +9,13 @@ def system_approval(random_token, input_form_id, form_id, workspace_id, approval
     airflow_url = org_mgr.get_airflow_url()
     token_json = {'input_from_id': input_form_id, 'form_id': form_id, 'approval_order': approval_order,
                   'workspace_id': workspace_id, 'token': random_token}
+    print('airflow token_json:', token_json)
+    print('airflow url:', airflow_url)
     token = prpcrypt.encrypt(json.dumps(token_json))
     payload = {'token': token, 'input_from_id': input_form_id, 'form_id': form_id, 'workspace_id': workspace_id}
+
+    print('airflow payload:', payload)
+    return True
     retry = 0
     while retry < 3:
         try:
