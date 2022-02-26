49e24083d81a1f1fca9b250906dd6165c74dd65e
torrotitans <torrotitans>
2022-02-26 13:41:04 +0800

    debug usecase resource

--- engine/db/input_form/db_input_form_mgr.py
+++ engine/db/input_form/db_input_form_mgr.py
@@ -669,6 +669,8 @@ class DbInputFormMgr(DbBase):
                 values = ('0', )
                 sql = self.create_update_sql(db_name, 'usecaseResourceTable', fields, values, condition)
                 _ = self.updete_exec(conn, sql)
+            print('data: ', data, team_group != resource_info['TEAM_GROUP'], service_account != resource_info['SERVICE_ACCOUNT'])
+            print('group: ', team_group, resource_info['TEAM_GROUP'], service_account, resource_info['SERVICE_ACCOUNT'])
             print('usecaseResourceTable updete_exec sql:', sql)
 
         return data
