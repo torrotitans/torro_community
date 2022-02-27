6197d764b9aef50c531d85bca4ffccb8ffc5f1e1
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-02-27 21:57:46 +0800

    Update main.py

--- engine/main.py
+++ engine/main.py
@@ -9,16 +9,6 @@ import os
 from api import create_app
 from config import configuration
 
-import logging
-from logging import config
-
-logging.config.fileConfig(fname='log.conf', disable_existing_loggers=False)
-
-# Get the logger specified in the file
-logger = logging.getLogger()
-
-logging.log("Starting the Torro Engine Sequence")
-
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 # CORS(app, supports_credentials=True)
