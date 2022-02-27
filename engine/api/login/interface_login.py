5ceb617fe7e4d7b08bffe1c976d442b2d5946646
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-02-27 22:24:03 +0800

    Update interface_login.py

--- engine/api/login/interface_login.py
+++ engine/api/login/interface_login.py
@@ -11,11 +11,15 @@ from common.common_response_process import response_result_process, DateEncoder
 from db.user.db_user_parameter import userApiPara
 from utils.api_version_verify import api_version
 from utils.auth_helper import Auth
-from utils.log_helper import lg
 from utils.status_code import response_code
 import json
 from config import config
 import os
+import logging
+import traceback
+
+# Get the logger specified in the file
+logger = logging.getLogger("main.api." + __name__)
 config_name = os.getenv('FLASK_CONFIG') or 'default'
 Config = config[config_name]
 
@@ -34,18 +38,21 @@ class interfaceLogin(Resource):
             if not request_data:
                 data = response_code.REQUEST_PARAM_MISSED
                 return response_result_process(data, xml=xml)
-            request_data = req.verify_all_param(request_data, userApiPara.login_POST_request)
+            
+            # Disable if the login ID is numeric 
+            # request_data = req.verify_all_param(request_data, userApiPara.login_POST_request)
 
             login_name, login_password = request_data.get('login_name'), request_data.get('login_password')
             offline_flag = request_data.get('is_offline', Status.offline_flag)
-            # # print('login: ', login_name, login_password)
-            # 对登录情况进行验证
+
+            # AuthN User
             dict_user = Auth.authenticate(login_name, login_password, offline_flag)
-            print('FN:interfaceLogin_post dict_user:', dict_user)
+            logger.debug('FN:interfaceLogin_post dict_user:', dict_user)
+            
             if 'token' not in dict_user:
                 return response_code.LOGIN_FAIL
-            # exit(0)
-            # 将用户信息写到全局
+
+            # Populate user data
             user_key = dict_user.get('ID')
             account_id = dict_user.get('ACCOUNT_ID')
             token = dict_user['token']
@@ -54,6 +61,7 @@ class interfaceLogin(Resource):
             # operation_log(description='login')
             if user_key:
                 g.user_key = user_key
+                logger.info("FN: User:{} Login:True".format(g.user_key))
                 g.account_id = account_id
                 data = response_code.SUCCESS
                 data['data'] = dict_user
@@ -63,26 +71,30 @@ class interfaceLogin(Resource):
             resp = make_response(json.dumps(data, cls=DateEncoder))
             origin = request.headers.get('Origin')
             # print('request.headers:', request.headers.get('Origin'))
+            
             if origin in self.allow_origins:
                 resp.headers['Access-Control-Allow-Origin'] = origin
             # resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
             resp.headers["Access-Control-Allow-Headers"] = "Content-Type"
             resp.headers['Access-Control-Allow-Credentials'] = 'true'
             resp.headers['Access-Control-Allow-Methods'] = "GET,POST,PUT,DELETE,OPTIONS"
+            
             if config_name == 'production':
                 resp.headers.add('Set-Cookie', 'token={}; SameSite=None; Secure'.format(token))
             else:
                 resp.set_cookie("token", token)
                 resp.set_cookie("SameSite", 'None', samesite=None, secure=None)
                 resp.set_cookie("Secure", samesite=None, secure=None)
-
-
+                
             return resp
+        
         except Exception as e:
-            import traceback
-            lg.error(traceback.format_exc())
+
+            logger.error(traceback.format_exc())
             error_data = response_code.LOGIN_FAIL
+            
             return response_result_process(error_data, xml=xml)
+        
     def put(self):
         xml = request.args.get('format')
         try:
@@ -126,9 +138,10 @@ class interfaceLogin(Resource):
                 resp = make_response(json.dumps(data, cls=DateEncoder))
                 resp.set_cookie("token", '')
                 return resp
+            
         except Exception as e:
-            # lg.error(e)
-            import traceback
-            print(traceback.format_exc())
+
+            logger.error(traceback.format_exc())
             error_data = response_code.LOGIN_FAIL
+            
             return response_result_process(error_data, xml=xml)
