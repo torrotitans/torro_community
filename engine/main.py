04f5fa2e663ed5201b76d9373af5e5deea67eaa1
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-02-27 22:09:28 +0800

    Update main.py

--- engine/main.py
+++ engine/main.py
@@ -8,6 +8,9 @@ import os
 # from flask_cors import CORS
 from api import create_app
 from config import configuration
+from logging import config
+
+logging.config.fileConfig(fname='log.conf', disable_existing_loggers=False)
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
