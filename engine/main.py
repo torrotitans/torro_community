5db75404ebc3f2279df214dedcef0c139e76f794
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-02-27 21:54:56 +0800

    Update main.py

--- engine/main.py
+++ engine/main.py
@@ -8,6 +8,7 @@ import os
 # from flask_cors import CORS
 from api import create_app
 from config import configuration
+
 import logging
 from logging import config
 
@@ -16,7 +17,10 @@ logging.config.fileConfig(fname='log.conf', disable_existing_loggers=False)
 # Get the logger specified in the file
 logger = logging.getLogger()
 
+logging.log("Starting the Torro Engine Sequence")
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
 # CORS(app, supports_credentials=True)
 if __name__ == '__main__':
     host, port, debug = configuration.get_start_config()
