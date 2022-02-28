befa8113f223e9986fd529957a531e9f2fa938f2
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-02-28 12:38:07 +0800

    Enhance the debugger

--- engine/db/dashboard/db_dashboard_mgr.py
+++ engine/db/dashboard/db_dashboard_mgr.py
@@ -6,9 +6,11 @@ from db.base import DbBase
 from db.connection_pool import MysqlConn
 import json
 from utils.status_code import response_code
-from utils.log_helper import lg
 from common.common_input_form_status import status
 # from utils.ldap_helper import Ldap
+import logging
+
+logger = logging.getLogger("main.utils." + __name__)
 
 class DbDashboardMgr(DbBase):
 
@@ -16,9 +18,6 @@ class DbDashboardMgr(DbBase):
     key_mapping = {'approver_id': 'account_id', 'id': 'inputFormTable.id'}
 
     def get_data(self, user_id, condition_dict, workspace_id=None):
-        """
-        :return:
-        """
         db_conn = MysqlConn()
         try:
             db_name = configuration.get_database_name()
@@ -42,10 +41,10 @@ class DbDashboardMgr(DbBase):
             group_list = []
             for ad_group in ad_group_infos:
                 group_list.append(ad_group['GROUP_MAIL'])
-            # print('group_list:', group_list)
+            # logger.debug("FN:get_data group_list:{}".format(group_list))
             user_info['GROUP_LIST'] = group_list
             adgroup_list = user_info['GROUP_LIST'] + [account_id]
-            # # print('user_info', adgroup_list)
+            # logger.debug("FN:get_data adgroup_list:{}".format(adgroup_list))
             # fetch inputFormTable info
             if 'approverView' in condition_dict:
                 approverView = True
@@ -56,7 +55,7 @@ class DbDashboardMgr(DbBase):
             max_history_condiction = ' GROUP BY id'
             fields = 'id,max(history_id)'
             max_history_sql = self.create_select_sql(db_name, table_name, fields, max_history_condiction).replace('where ', '')
-            # print('max_history_condiction sql: ', max_history_sql)
+            # logger.debug("FN:get_data max_history_sql:{}".format(max_history_sql))
             role_relations = [{"table_name": "inputFormIndexTable", "join_condition": "inputFormIndexTable.id=approvalTable.input_form_id"},
                               {"table_name": "inputFormTable", "join_condition": "inputFormTable.id=approvalTable.input_form_id"}]
             condition_list = []
@@ -72,7 +71,7 @@ class DbDashboardMgr(DbBase):
                         form_status_length = len(condition_dict['form_status'])
                     for i in range(form_status_length):
                         form_status.append(int(condition_dict['form_status'][i][0]))
-                    print('form_status:', form_status)
+                    logger.debug("FN:get_data form_status:{}".format(form_status))
                     if status.pending_approval in form_status:
                         condition_list.append("(now_approval=1)")
                     else:
@@ -80,13 +79,13 @@ class DbDashboardMgr(DbBase):
                     if status.approved in form_status:
                         condition_dict['form_status'] = [[status.completed, '=']] + condition_dict['form_status']
                         condition_list.append("(approvalTable.is_approved=1)")
-                    print('condition_dict:', condition_dict['form_status'] )
+                    logger.debug("FN:get_data condition_dict:{}".format(condition_dict['form_status']))
                 else:
                     condition_list.append("(approvalTable.account_id='{}')".format(account_id))
             else:
                 condition = "(inputFormTable.id, history_id) in (%s) and inputFormIndexTable.creator_id='%s'" % (max_history_sql, user_id)
-
-            # # print(condition)
+                
+            # logger.debug("FN:get_data condition:{}".format(condition))
             condition_list.append("(inputFormIndexTable.workspace_id='%s')" % workspace_id)
             if condition_dict:
                 tamplate = "{key}{cond}'{value}'"
@@ -111,15 +110,15 @@ class DbDashboardMgr(DbBase):
             condition += ' and '
             condition += ' and '.join(condition_list)
             condition += ' order by inputFormTable.create_time desc'
-            # print('condition:', condition)
+            # logger.debug("FN:get_data condition:{}".format(condition))
             fields = 'inputFormTable.id,history_id,form_id,workflow_id,creator_id,account_id as approver_id,workflow_name,fields_num,stages_num,form_status,ad_group,inputFormTable.create_time,inputFormTable.updated_time,inputFormTable.form_field_values_dict'
             role_name_query_sql = self.create_get_relation_sql(db_name, "approvalTable", fields, role_relations,
                                                                condition=condition)
-            # print('role_name_query_sql: ', role_name_query_sql)
+            # logger.debug("FN:get_data role_name_query_sql:{}".format(role_name_query_sql))
             # exit(0)
 
             # get usecase field info
-            # print(status.system_form_id, workspace_id)
+            # logger.debug("FN:get_data system_form_id:{} workspace_id:{}".format(status.system_form_id, workspace_id))
             dy_condition = "form_id=%s" % (status.system_form_id['usecase'])
             sql = self.create_select_sql(db_name, 'dynamicFieldTable',
                                          'id,label',
@@ -142,7 +141,8 @@ class DbDashboardMgr(DbBase):
             #         dy_label_value_dict[option_value] = option_label
             # print('dy_label_value_dict:', dy_label_value_dict)
             # fetch duplicate form
-            print('role_name_query_sql:', role_name_query_sql)
+            
+            logger.debug("FN:get_data role_name_query_sql:{}".format(role_name_query_sql))
             result = self.execute_fetch_all(db_conn, role_name_query_sql)
             return_result = []
             return_result_keys = []
@@ -159,13 +159,13 @@ class DbDashboardMgr(DbBase):
                     condition = 'ID="%s"' % user_id
                     user_fields = 'ACCOUNT_ID'
                     sql = self.create_select_sql(db_name, 'userTable', user_fields, condition=condition)
-                    print('user Table:', sql)
+                    logger.debug("FN:get_data userTable_sql:{}".format(sql))
                     user_info = self.execute_fetch_one(db_conn, sql)
                     raw_result['creator_id'] = user_info['ACCOUNT_ID']
                     # get uasecase field value
                     form_field_values_dict = json.loads(raw_result['form_field_values_dict'])
                     raw_result[dy_field_label] = ''
-                    # print('form_field_values_dict:', form_field_values_dict, dy_field_id)
+                    # logger.debug("FN:get_data form_field_values_dict:{} dy_field_id:{}".format(form_field_values_dict, dy_field_id))
                     if dy_field_id in form_field_values_dict:
                         dy_label_value = form_field_values_dict[dy_field_id]['value']
                         # if dy_value in dy_label_value_dict:
@@ -180,10 +180,11 @@ class DbDashboardMgr(DbBase):
             return data
         except Exception as e:
             import traceback
-            lg.error(traceback.format_exc())
+            logger.error(traceback.format_exc())
             return response_code.GET_DATA_FAIL
         finally:
             db_conn.close()
+            
     def get_options(self, user_id, workspace_id):
 
         db_conn = MysqlConn()
@@ -196,9 +197,9 @@ class DbDashboardMgr(DbBase):
             ]
             sql = self.create_get_relation_sql(db_name, 'user_to_adgroupTable', 'user_to_adgroupTable.AD_GROUP_ID', relations=relation_tables,
                                                condition=condition)
-            # print('create_get_relation_sql1:', sql)
+            # logger.debug("FN:get_options user_to_adgroupTable_sql:{}".format(sql))
             ad_group_infos = self.execute_fetch_all(db_conn, sql)
-            # print('ad_group_infos:', ad_group_infos)
+            # logger.debug("FN:get_options ad_group_infos:{}".format(ad_group_infos))
             group_id_set = set()
             for ad_group in ad_group_infos:
                 group_id_set.add(str(ad_group['AD_GROUP_ID']))
@@ -211,13 +212,14 @@ class DbDashboardMgr(DbBase):
                 sql = self.create_get_relation_sql(db_name, 'user_to_adgroupTable', 'userTable.ID, ACCOUNT_NAME, ACCOUNT_ID',
                                                    relations=relation_tables,
                                                    condition=condition)
-                # print('create_get_relation_sql2:', sql)
+                # logger.debug("FN:get_options user_to_adgroupTable_sql:{}".format(sql))
                 user_infos = self.execute_fetch_all(db_conn, sql)
-                # print('user_infos:', user_infos)
+                # logger.debug("FN:get_options user_infos:{}".format(user_infos))
                 for user_info in user_infos:
                     creators.append({'label': user_info['ACCOUNT_ID'], 'value': user_info['ID']})
             condition = 'workspace_id = "%s"' % workspace_id
             sql = self.create_select_sql(db_name, 'formTable', '*', condition)
+            # logger.debug("FN:get_options formTable_sql:{}".format(sql))
             form_infos = self.execute_fetch_all(db_conn, sql)
             formList = []
             for form_info in form_infos:
@@ -225,9 +227,10 @@ class DbDashboardMgr(DbBase):
             data = response_code.SUCCESS
             data['data'] = {'creator': creators, 'formList': formList}
             return data
+        
         except Exception as e:
             import traceback
-            lg.error(traceback.format_exc())
+            logger.error(traceback.format_exc())
             return response_code.GET_DATA_FAIL
         finally:
             db_conn.close()
@@ -254,25 +257,30 @@ class DbDashboardMgr(DbBase):
             ]
             sql = self.create_get_relation_sql(db_name, 'inputNotifyTable', 'inputNotifyTable.*, formTable.title', relations=relation_tables,
                                                condition=condition)
-            print('inputNotifyTable sql:', sql)
+            # logger.debug("FN:get_options inputNotifyTable_sql:{}".format(sql))
             return_notify_infos = self.execute_fetch_all(db_conn, sql)
 
             # return_notify_infos = self.execute_fetch_all(db_conn, sql)
             if isinstance(return_notify_infos, list):
                 notify_infos.extend(return_notify_infos)
+                
             notify_id_list = list()
             return_data = []
+            
             for notify_info in notify_infos:
                 id = notify_info['id']
+                
                 if id not in notify_id_list:
                     notify_id_list.append(id)
                     return_data.append(notify_info)
+                    
             data = response_code.SUCCESS
             data['data'] = return_data
             return data
+        
         except Exception as e:
             import traceback
-            lg.error(traceback.format_exc())
+            logger.error(traceback.format_exc())
             return response_code.GET_DATA_FAIL
         finally:
             db_conn.close()
@@ -295,13 +303,15 @@ class DbDashboardMgr(DbBase):
             fields = ('is_read', )
             values = (is_read, )
             sql = self.create_update_sql(db_name, 'inputNotifyTable', fields, values, condition=condition)
-            print("FN:read_notify sql:",sql)
+            logger.debug("FN:read_notify inputNotifyTable_sql:{}".format(sql))
             _ = self.updete_exec(db_conn, sql)
             return response_code.SUCCESS
+        
         except Exception as e:
             import traceback
-            lg.error(traceback.format_exc())
+            logger.error(traceback.format_exc())
             return response_code.GET_DATA_FAIL
+        
         finally:
             db_conn.close()
 
