b31106fd242282a47d90675a2d55c4e589785497
torrotitans <95328648+torrotitans@users.noreply.github.com>
2022-03-01 18:18:25 +0800

    Update airflow_helper.py

--- engine/utils/airflow_helper.py
+++ engine/utils/airflow_helper.py
@@ -2,6 +2,7 @@ import requests
 import json
 from common.common_crypto import prpcrypt
 from db.org.db_org_mgr import org_mgr
+import traceback
 import logging
 
 logger = logging.getLogger("main." + __name__)
@@ -19,11 +20,11 @@ def system_approval(random_token, input_form_id, form_id, workspace_id, approval
         payload = {'token': token, 'input_form_id': input_form_id, 'form_id': form_id, 'workspace_id': workspace_id}
         payload = json.dumps(payload)
 
-        logger.info('FN:system_approval airflow_url:{} payload:{}'.format(airflow_url,payload))
         # return True
         retry = 0
         while retry < 3:
             try:
+                logger.info('FN:system_approval airflow_url:{} payload:{}'.format(airflow_url,payload))
                 res = requests.post(airflow_url, data=payload, verify=False, header=airflow_header)
                 logger.info("FN:system_approval airflow_response_status_code:{} airflow_response_text:{}".format(res.status_code,res.text))
                 
@@ -33,7 +34,8 @@ def system_approval(random_token, input_form_id, form_id, workspace_id, approval
                 else:
                     retry += 1    
                 
-            except:
+            except Exception as e:
+                logger.error("FN:system_approval error:{}".format(traceback.format_exc()))
                 retry += 1
                 
             logger.info("FN:system_approval airflow_trigger_success:False")
