7fed7d93a9d3ddbe0c203704722f19ff86e4c0e9
torrotitans <titans@torro.ai>
2022-04-25 12:11:26 +0800

    autp disappear system-notify

--- server/src/utils/systerm-error.js
+++ server/src/utils/systerm-error.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import GlobalNotification from "@basics/GlobalNotification";
 
 const ID = "system-error";
-
+let closeTimer = null;
 export const closeNotify = () => {
   const el = document.getElementById(ID);
   if (!el) {
@@ -19,22 +19,30 @@ export const closeNotify = () => {
 };
 
 export const sendNotify = (options = {}) => {
+  clearTimeout(closeTimer);
+  closeTimer = null;
+
   if (document.getElementById(ID)) {
     ReactDOM.render(
       <GlobalNotification handleClose={closeNotify} {...options} />,
       document.getElementById(ID)
     );
-    return;
+  } else {
+    const el = document.createElement("div");
+    el.id = ID;
+    ReactDOM.render(
+      <GlobalNotification handleClose={closeNotify} {...options} />,
+      el
+    );
+    const parent = document.querySelector("#root");
+    parent.insertBefore(el, parent.firstChild);
   }
 
-  const el = document.createElement("div");
-  el.id = ID;
-  ReactDOM.render(
-    <GlobalNotification handleClose={closeNotify} {...options} />,
-    el
-  );
-  const parent = document.querySelector("#root");
-  parent.insertBefore(el, parent.firstChild);
+  closeTimer = setTimeout(() => {
+    closeNotify();
+    clearTimeout(closeTimer);
+    closeTimer = null;
+  }, 5000);
 };
 
 export default {
